6308625a7f6853fda894eabb22931f44682f2219
Alexandr Akulich <akulichalexander@gmail.com>
2017-07-03 23:32:43 +0300

    TelegramUtils: Add TelegramUserFlag enumeration

--- TelegramQt/TelegramUtils.hpp
+++ TelegramQt/TelegramUtils.hpp
@@ -36,6 +36,29 @@ namespace TelegramUtils
         TelegramMessageFlagForward = 1 << 2,
         TelegramMessageFlagReply   = 1 << 3,
     };
+
+    enum TelegramUserFlag {
+        TelegramUserFlagNone = 0,
+        TelegramUserFlagAccessHash = 1 << 0, // 1, AccessHash
+        TelegramUserFlagFirstName = 1 << 1, // 2, First name
+        TelegramUserFlagLastName = 1 << 2, // 4, Last name
+        TelegramUserFlagUserName = 1 << 3, // 8, User name
+        TelegramUserFlagPhone = 1 << 4, // 16, Phone
+        TelegramUserFlagHasPhoto = 1 << 5, // 32, Photo
+        TelegramUserFlagStatus = 1 << 6, // 64, Status
+        TelegramUserFlagSelf = 1 << 10, // 1024
+        TelegramUserFlagIsContact = 1 << 11, // 2048
+        TelegramUserFlagIsMutualContact = 1 << 12, // 4096
+        TelegramUserFlagDeleted = 1 << 13, // 8192
+        TelegramUserFlagBot = 1 << 14, // 16384, Bot with version
+        TelegramUserFlagBotChatHistory = 1 << 15, // 32768
+        TelegramUserFlagBotNoChats = 1 << 16, // 65536
+        TelegramUserFlagVerified = 1 << 17, // 131072
+        TelegramUserFlagRestricted = 1 << 18, // 262144, Restricted with a reason
+        TelegramUserFlagBotInlinePlaceholder = 1 << 19, // 524288, Bot with placeholder
+        TelegramUserFlagMin = 1 << 20, // 1048576
+        TelegramUserFlagBotInlineGeo = 1 << 21, // 2097152
+    };
 }
 
 #endif // CTELEGRAMUTILS_HPP
